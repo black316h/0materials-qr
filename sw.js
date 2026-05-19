4562c72d643e54dd80c73c2435ab48b156fbcecb
black316h <42133665+black316h@users.noreply.github.com>
2026-05-19 16:44:10 +0800

    add service worker

---
+++ sw.js
@@ -0,0 +1,36 @@
+var CACHE_NAME = 'hongi-v1';
+var urlsToCache = [
+  '/0materials-qr/',
+  '/0materials-qr/index.html',
+  '/0materials-qr/icon-192.png',
+  '/0materials-qr/icon-512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n) { return n !== CACHE_NAME; })
+             .map(function(n) { return caches.delete(n); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
